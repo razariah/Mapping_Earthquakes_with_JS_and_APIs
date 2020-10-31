9a8a8dfb440d1cfd737b14100855f9dff2939d30
rameshazariah <ramesh@allcareplus.org>
2020-10-30 21:13:14 -0700

    GeoJson points

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,19 @@
+// We create the street view tile layer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
+
+streets.addTo(map);
+
+let airportData = "https://raw.githubusercontent.com/razariah/Mapping_Earthquakes_with_JS_and_APIs/Mapping_GeoJSON_Points/Mapping_GeoJSON_Points/majorAirports.json"
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
